a18e04b558580f58c750d1ff5b5367902e4e5e96
rupambharati39 <rupambharati39@gmail.com>
2025-01-18 09:34:11 +0530

    driver class updated

--- src/test/java/Driver.java
+++ src/test/java/Driver.java
@@ -1,5 +1,6 @@
 public class Driver {
     public static void main(String[] args) throws InterruptedException {
+        //this is the driver class
         SeleniumTutorial obj = new SeleniumTutorial();
         obj.launchBrowserAndApplication();
     }
